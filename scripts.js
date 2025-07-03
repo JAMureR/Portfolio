8a1e8b3db4b52f8fb6c1e6c4cb8be5a2b376e24b
JAMureR <jmuresat@gmail.com>
2025-07-03 20:56:18 +0100

    Finalizada funcion flip en targetas de proyecto

--- scripts.js
+++ scripts.js
@@ -48,3 +48,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
   observer.observe(seccionProyectos);
 });
+
+document.querySelectorAll('.card-proyecto').forEach(card => {
+    card.addEventListener('click', () => {
+      const inner = card.querySelector('.card-inner');
+      inner.classList.toggle('flipped');
+    });
+  });
